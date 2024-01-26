e330109e4588882c47e6e0a2d33e9ecb8c514a08
TheoBM5 <71059430+TheoBM5@users.noreply.github.com>
2024-01-26 11:45:18 -0600

    Creacion del backend, conexion a bd

---
+++ backend/database/mind.sql
@@ -0,0 +1,44 @@
+CREATE TABLE card(
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(255) UNIQUE NOT NULL,
+    description TEXT
+);
+
+ALTER TABLE task ADD COLUMN user_id INTEGER REFERENCES users(id);
+
+CREATE TABLE user_deck(
+    id SERIAL PRIMARY KEY,
+)
+
+ALTER TABLE user_deck ADD COLUMN user_id INTEGER REFERENCES users(id);
+ALTER TABLE user_deck ADD COLUMN deck_id INTEGER REFERENCES users(id);
+
+
+CREATE TABLE users(
+    id SERIAL PRIMARY KEY,
+    user VARCHAR(255) UNIQUE NOT NULL,
+    name VARCHAR(255) NOT NULL,
+    apellido VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+)
+
+ALTER TABLE users ADD COLUMN gravatar VARCHAR(255);
+
+CREATE TABLE deck(
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    type VARCHAR(255) NOT NULL,
+    description VARCHAR(255)
+)
+
+CREATE TABLE parameters(
+    id SERIAL PRIMARY KEY,
+    racha
+    ef
+    interval_repeat
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    
+)
---
+++ backend/src/app.js
@@ -0,0 +1,35 @@
+import express from 'express'
+import morgan from 'morgan'
+import taskRoutes from './routes/tasks.routes.js';
+import authRoutes from './routes/auth.routes.js';
+import cookieParse from 'cookie-parser';
+import cors from 'cors';
+const app=express();
+
+//Middlewares
+app.use(
+    cors({
+        origin: 'http://localhost:5173',
+        credentials: true,
+})
+);
+app.use(morgan('dev'));
+app.use(cookieParse());
+app.use(express.json());
+app.use(express.urlencoded({extended:false}));
+
+//Routes
+app.get("/", (req, res) => res.json({message: "welcome to my API"}));
+app.use('/api', taskRoutes);
+app.use('/api', authRoutes);
+
+
+//Error Hander
+app.use((err, req, res, next)=>{
+    res.status(500).json({
+        status: "error",
+        message: err.message,
+    });
+});
+
+export default app;
